b09e4ee596dabb90f856498fd2216ecb14b11410
강지행 <jey78v@naver.com>
2023-07-04 20:21:15 +0900

    7월4일 노트북 블로그

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Gangsabar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+ <p>봤냐?</p>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//7월4일 Blog 프로젝트 생성과 뷰에서 Bootstrap 4, 5 사용법
+//Bootstrap 사이트에서 introduction에서 찾아서 css와 js 파일 링크 붙이면됨  
+//=>publc의 index.html에 css는 head에 js는 body 끝나기 전에 
+//npm으로 설치가능 terminal 완전 종료후에 최신버전 설치명령어 =>npm install bootstrap@latest
+//잘 설치되면 <template>에 붙여넣으면 됨 
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -21,6 +49,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
 }
 </style>
